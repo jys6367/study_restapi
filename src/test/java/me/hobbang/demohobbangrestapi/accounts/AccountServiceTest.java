4902875308a477e907ee6cc561b2ea1c90031ecf
hobbang <jys6367>
2020-04-12 21:40:15 +0900

    예외 테스트

--- src/test/java/me/hobbang/demohobbangrestapi/accounts/AccountServiceTest.java
+++ src/test/java/me/hobbang/demohobbangrestapi/accounts/AccountServiceTest.java
@@ -1,10 +1,14 @@
 package me.hobbang.demohobbangrestapi.accounts;
 
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,11 +17,15 @@ import javax.validation.constraints.AssertTrue;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class AccountServiceTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Autowired
     AccountService accountService;
@@ -46,6 +54,15 @@ public class AccountServiceTest {
         assertThat(userDetails.getPassword()).isEqualTo(password);
     }
 
+    @Test
+    public void findByUsernameFail() {
+        // Expected
+        String username = "random@email.com";
+        // expectedException은 실제 exception보다 위에 작성되야함.
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
 
-
+        // When
+        accountService.loadUserByUsername(username);
+    }
 }
