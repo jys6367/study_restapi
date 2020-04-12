8f733c18dc2d0a47c11369c908b78a0fe6684d9e
hobbang <jys6367>
2020-04-12 22:10:00 +0900

    스프링 시큐리티 폼 인증 설정

--- src/main/java/me/hobbang/demohobbangrestapi/configs/AppConfig.java
+++ src/main/java/me/hobbang/demohobbangrestapi/configs/AppConfig.java
@@ -1,11 +1,19 @@
 package me.hobbang.demohobbangrestapi.configs;
 
+import me.hobbang.demohobbangrestapi.accounts.Account;
+import me.hobbang.demohobbangrestapi.accounts.AccountRole;
+import me.hobbang.demohobbangrestapi.accounts.AccountService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.ui.ModelMap;
+
+import java.util.Set;
 
 @Configuration
 public class AppConfig {
@@ -18,5 +26,25 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+
+            @Autowired
+            AccountService accountService;
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                Account hobbang = Account.builder()
+                        .email("hobbang@email.com")
+                        .password("hobbang")
+                        .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                        .build();
+
+                accountService.saveAccount(hobbang);
+            }
+        };
     }
 }
